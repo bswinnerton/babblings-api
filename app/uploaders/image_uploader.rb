1f76edbb98bc601e8a6df876a935ee76bc6a07fa
Brooks Swinnerton <bswinnerton@gmail.com>
2015-09-12 12:45:24 -0400

    Adds support for dimension peristence

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  process :store_dimensions
+
   version(:medium)  { process resize_to_fill: [600, 600] }
   version(:small)   { process resize_to_fill: [300, 300] }
 
@@ -10,5 +12,16 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   def cache_dir
     "uploads/#{ENV.fetch('RACK_ENV')}/images/tmp/"
+  end
+
+  private
+
+  def store_dimensions
+    return unless file && model
+    return unless model.attributes.keys.include? 'width'
+    return unless model.attributes.keys.include? 'height'
+
+    height, width = MiniMagick::Image.open(file.file).dimensions
+    model.update(height: height, width: width)
   end
 end
--- spec/uploaders/image_uploader_spec.rb
+++ spec/uploaders/image_uploader_spec.rb
@@ -1,9 +1,23 @@
 require 'spec_helper'
 
+Model = Class.new do
+  ATTRS = %w(width height)
+  attr_accessor *ATTRS.map(&:to_sym)
+
+  def attributes
+    OpenStruct.new(keys: ATTRS)
+  end
+
+  def update(attrs)
+    @width = attrs.fetch(:width)
+    @height = attrs.fetch(:height)
+  end
+end
+
 RSpec.describe ImageUploader do
   let(:placeholder_image) { 'spec/support/carrierwave_placeholder.png' }
 
-  subject { ImageUploader.new }
+  subject { ImageUploader.new(Model.new) }
 
   before do
     ImageUploader.enable_processing = true
@@ -12,15 +26,27 @@ RSpec.describe ImageUploader do
     end
   end
 
-  context 'medium' do
-    it 'scales down an image to 600x600' do
-      expect(subject.medium).to be_no_larger_than(600, 600)
+  context 'resizing' do
+    context 'medium' do
+      it 'scales down an image to 600x600' do
+        expect(subject.medium).to be_no_larger_than(600, 600)
+      end
+    end
+
+    context 'small' do
+      it 'scales down an image to 300x300' do
+        expect(subject.medium).to be_no_larger_than(600, 600)
+      end
     end
   end
 
-  context 'small' do
-    it 'scales down an image to 300x300' do
-      expect(subject.medium).to be_no_larger_than(600, 600)
+  context 'dimension persistence' do
+    it 'persists the width' do
+      expect(subject.model.width).to eq 1000
+    end
+
+    it 'persists the height' do
+      expect(subject.model.height).to eq 1000
     end
   end
 end
