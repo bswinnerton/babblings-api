603842cd02d42981c980e394dccd5d749b3e6362
Brooks Swinnerton <bswinnerton@gmail.com>
2016-06-12 14:54:51 -0400

    Resolve ActiveRecord finder :bomb:s

--- app/graph/node_identification.rb
+++ app/graph/node_identification.rb
@@ -2,7 +2,7 @@ module Graph
   NodeIdentification = GraphQL::Relay::GlobalNodeIdentification.define do
     object_from_id -> (id, context) do
       type_name, id = NodeIdentification.from_global_id(id)
-      type_name.constantize.find(id)
+      type_name.constantize.find_by(id: id)
     end
 
     type_from_object -> (object) do
