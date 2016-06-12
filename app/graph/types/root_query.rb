266a5f1c1122c9b31e2c958a7ec0598a08c31590
Brooks Swinnerton <bswinnerton@gmail.com>
2016-06-12 15:56:05 -0400

    Add image connection to root query

--- app/graph/types/root_query.rb
+++ app/graph/types/root_query.rb
@@ -7,6 +7,15 @@ module Graph
       description 'The query root.'
 
       field :node, :field => NodeIdentification.field
+
+      connection :images, -> { !Graph::Types::Image.connection_type } do
+        description 'Return all images'
+        type -> { Graph::Types::Image }
+
+        resolve -> (object, arguments, context) do
+          ::Image.all
+        end
+      end
     end
   end
 end
